0e1af2be0d2d37c33b6164856d7b68b75b79a989
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2022-07-22 15:34:06 +0200

    Player Sorting Group & Sorting ORder fixes, Weapon class proto

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,38 +4,72 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    private Animator anim;
-    private float fireRate = 0.1f;
-    private float fireLast = 0;
+    // SPRITES & ANIMATIONS
+    protected Animator anim;
+    protected SpriteRenderer sr;
+
+    protected float fireRate = 0.1f;
+    protected float fireTimer = 0;
+
+    // TIMER ARRAY
+    protected float[] wpnTimers;
+
+    // DIFFERENT WEAPON TIMERS ENUM
+    protected enum WeaponTimers {
+        fireTimer = 0,
+        burstTimer = 1,
+        end = 2
+    }
 
     // AWAKE
     void Awake()
     {
+        wpnTimers = new float[(int)WeaponTimers.end];
         anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // FIXED UPDATE
     void FixedUpdate()
     {
-        if (fireLast > 0)
-            fireLast -= Time.deltaTime;
+        UpdateTimers();
     }
 
     // UPDATE WEAPON ANIMATION STATE
-    public void UpdateWeapon(Vector3 moveDelta)
+    public void UpdateWeapon(Vector3 facingDir)
     {
-        anim.SetFloat("velX", moveDelta.x);
-        anim.SetFloat("velY", moveDelta.y);
+        if (anim != null && sr != null)
+        {
+            anim.SetFloat("velX", facingDir.x);
+            anim.SetFloat("velY", facingDir.y);
+            if (facingDir.y > Mathf.Abs(facingDir.x)) {
+                sr.sortingOrder = -1;
+            } else {
+                sr.sortingOrder = 1;
+            }
+        }
     }
 
-    // UPDATE FIRE TRIGGER
-
     // FIRE WEAPON
-    public void Fire(Vector3 origin)
+    public void Fire(Vector3 origin, Vector3 target)
     { 
-        if (fireLast <= 0) {
-            GameManager.boomPS.Emit(origin,Vector3.zero, Random.Range(0.75f,1.25f), 0.0625f, Color.white);
-            fireLast = fireRate;
+        if (!(wpnTimers[(int)WeaponTimers.fireTimer] > 0)) {
+            MuzzleFlash(origin);
+            wpnTimers[(int)WeaponTimers.fireTimer] = fireRate;
         }       
+    }
+
+    // DISPLAY MUZZLE FLASH
+    protected void MuzzleFlash(Vector3 origin)
+    {
+        GameManager.boomPS.Emit(origin,Vector3.zero, Random.Range(0.75f,1.25f), 0.0625f, Color.white);
+    }
+
+    // UPDATE FIRE TIMER
+    private void UpdateTimers()
+    {
+        for (int i = 0; i < wpnTimers.Length; i++){
+            if (wpnTimers[i] > 0f) wpnTimers[i] -= Time.deltaTime;
+        }
     }
 }
