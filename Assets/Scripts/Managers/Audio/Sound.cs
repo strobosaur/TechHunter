40cad5829a6bbcd0e1cce5a65a45742a70d18d5d
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2022-08-04 23:07:36 +0200

    Audio System Basics

---
+++ Assets/Scripts/Managers/Audio/Sound.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public AudioClip clip;
+    public string name;
+}
