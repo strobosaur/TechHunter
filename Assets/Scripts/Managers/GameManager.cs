193d5f068f9e09208adbc4ac60539cf17b1a373f
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2022-08-04 18:35:36 +0200

    Game Manager State Machine & Scene Structure Outlines

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -7,9 +7,21 @@ public class GameManager : MonoBehaviour
     // STATIC INSTANCE
     public static GameManager instance;
 
-    // PLAYER MANAGER
-    public PlayerManager playerManager;
+    // MANAGER STATE
+    public MenuManager menu;
 
+    // PERSISTENT MANAGERS
+    public PlayerManager playerManager;
+    public DisplayManager displayManager;
+    public MenuManager menuManager;
+
+    // MANAGER STATE MACHINE
+    public StateMachine stateMachine;
+    public StateManagerMenu stateMenu;
+    public StateManagerBase stateBase;
+    public StateManagerLevel stateLevel;
+
+    // AWAKE
     void Awake()
     {
         // CREATE SINGLETON
@@ -21,9 +33,36 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
 
+        // GET MANAGER COMPONENTS
         playerManager = GetComponent<PlayerManager>();
+        displayManager = GetComponent<DisplayManager>();
+        menuManager = GetComponent<MenuManager>();
+
+        // INIT STATE MACHINE
+        stateMachine = new StateMachine();
+        stateMenu = new StateManagerMenu(this, stateMachine);
+        stateBase = new StateManagerBase(this, stateMachine);
+        stateLevel = new StateManagerLevel(this, stateMachine);
 
         // DISABLE MOUSE CURSOR
         Cursor.visible = false;
+    }
+
+    // START
+    void Start()
+    {
+        stateMachine.Iinitialize(stateMenu);
+    }
+
+    // UPDATE
+    void Update()
+    {
+        stateMachine.CurrentState.LogicUpdate();
+    }
+
+    // FIXED UPDATE
+    void FixedUpdate()
+    {
+        stateMachine.CurrentState.PhysicsUpdate();
     }
 }
